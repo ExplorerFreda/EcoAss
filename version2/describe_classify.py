b2883603e78b7a1ff883a06a136909d98a0cba26
ExplorerFreda <hyshi@pku.edu.cn>
2016-12-25 13:35:14 +0800

    match taobao & alipay

---
+++ version2/describe_classify.py
@@ -0,0 +1,66 @@
+import codecs
+
+
+describe_filename = '../../data/describe.txt'
+cluster_filename = '../../data/kmeans_result_sorted_cluster.txt'
+output_filename = '../../data/describe_type.txt'
+flog = codecs.open('log.txt','w','utf8')
+
+# find corresponding category for each cluster
+cluster = dict()
+cluster_to_category = dict()
+for line in codecs.open(cluster_filename,'r','utf8'):
+	[word, clus] = line.split()
+	pos = word.rfind('/')
+	word = word[:pos]
+	clus = int(clus)
+	cluster[word] = clus
+cluster_category_score = [[0 for i in range(13)] for j in range(200)]
+for category in range(13):
+	wordlist_filename = '../../data/sample_words_new/%d.txt'%category
+	for line in codecs.open(wordlist_filename, 'r', 'utf8'):
+		pos = line.find('/')
+		word = line[:-2]
+		if word not in cluster:
+			continue
+		cluster_category_score[cluster[word]][category] += 1
+for i in range(200):
+	maxcnt = 0
+	argmax = -1
+	for j in range(13):
+		if maxcnt < cluster_category_score[i][j]:
+			maxcnt = cluster_category_score[i][j]
+			argmax = j
+	cluster_to_category[i] = argmax
+for line in open('cates.txt'):
+	clus, cate = line.split()
+	clus = int(clus)
+	cate = int(cate)
+	cluster_to_category[clus] = cate
+print cluster_to_category
+
+fout = codecs.open(output_filename,'w','utf8')
+
+for cnt, line in enumerate(codecs.open(describe_filename,'r','utf8')):
+	words = line.split()
+	newline = ''
+	weight = [0 for i in range(13)]
+	for word in words:
+		word = word[:word.rfind('/')]
+		newline += word
+		if word in cluster and cluster_to_category[cluster[word]]!=-1:
+			weight[cluster_to_category[cluster[word]]] += 1
+	maxcnt = 0
+	argmax = -1
+	for i in range(13):
+		if maxcnt < weight[i]:
+			maxcnt = weight[i]
+			argmax = i
+	if cnt % 1000 == 0:
+		print cnt
+	if newline == '':
+		continue
+	fout.write(newline + ' ' + str(argmax) + '\n')
+
+fout.close()
+flog.close()
---
+++ version2/match_taobao_and_alipay.py
@@ -0,0 +1,114 @@
+import json
+import codecs
+
+output_filename = '../../data/taobao_result_new_matched.csv'
+input_filename = '../../data/taobao_result_new.dat'
+describe_type_filename = '../../data/describe_type.txt'
+
+fout = codecs.open(output_filename,'w','utf8')
+fout.write('"id","time","money","status","alpay_desc","tb_desc","tb_type","tb_quantity","tb_province","tb_city","alipay_count","tb_count","huabei_available","taobaoLevel","huabei_quota","category"\n')
+flog = codecs.open('log.txt', 'w', 'utf8')
+
+describe_type = dict()
+def load_describe_type(filename):
+	for line in codecs.open(filename, 'r', 'utf8'):
+		try:
+			[item, cate] = line.split()
+		except:
+			print line
+		describe_type[item] = cate
+
+load_describe_type('../../data/describe_type.txt')
+print 'loaded describe type'
+
+def analysis(taobao_orders, alipay_bills, cid, taobaoLevel, huabeiedu, huabeiAvailable):
+	ret = False
+	for it in alipay_bills:
+		pos = it['status'].find(' ')
+		if pos != -1:
+			it['status'] = it['status'][:pos]
+	black_namelist = set()
+	for item in taobao_orders:
+		taobao_desc = item['describe']
+		if type(item['province']) != type(u'string'):
+			item['province'] = str(item['province'])
+		if type(item['city']) != type(u'string'):
+			item['city'] = str(item['city'])
+		if type(huabeiedu) != type('string'):
+			huabeiedu = str(huabeiedu)
+		if type(huabeiAvailable) != type('string'):
+			huabeiAvailable = str(huabeiAvailable)
+		if type(taobaoLevel) != type(u'string'):
+			taobaoLevel = str(taobaoLevel)
+		taobao_count = 0
+		taobao_trans_time = item['trans_time'][:-3]
+		if (taobao_desc, taobao_trans_time, item['price']) in black_namelist:
+			continue
+		else:
+			black_namelist.add((taobao_desc, taobao_trans_time, item['price']) )
+		for i in range(len(taobao_orders)):
+			if taobao_orders[i]['describe'] == taobao_desc and taobao_orders[i]['trans_time'][:-3] == taobao_trans_time:
+				taobao_count += 1
+		successful_matched = False
+		flog.write('taobao ' + taobao_trans_time + '\n')
+		for it in alipay_bills:
+			alipay_trans_time = it['trans_time'][:-3]
+			if taobao_trans_time == alipay_trans_time and -it['money'] == item['price']:
+				if taobao_desc not in describe_type:
+					continue
+				alipay_desc = it['trans_desc']
+				alipay_count = 0
+				for i in range(len(alipay_bills)):
+					if alipay_bills[i]['trans_desc'] == alipay_desc and alipay_bills[i]['trans_time'][:-3] == alipay_trans_time:
+						alipay_count += 1
+				fout.write('"'+str(cid)+'",' # cid
+				+'"'+taobao_trans_time+'",' # time
+				+'"'+str(item['price'])+'",' # money
+				+'"'+it['status']+'",' # status
+				+'"'+it['trans_desc']+'",' # ali_desc
+				+'"'+item['describe']+'",' # tb_desc
+				+'"'+item['type']+'",' # type
+				+'"'+str(item['quantity'])+'",' #quantity
+				+'"'+item['province']+'",' # tb_province
+				+'"'+item['city']+'",' # tb_city
+				+'"'+str(taobao_count)+'",' # alipay_count
+				+'"'+str(alipay_count)+'",' # tb_count
+				+'"'+huabeiAvailable+'",' #huabei_available
+				+'"'+taobaoLevel+'",' # taobaoleve;
+				+'"'+huabeiedu+'",' # huabeiedu
+				+'"'+describe_type[taobao_desc]+'"' #category
+				+'\n')
+				# print taobao_trans_time, alipay_trans_time, it['money'], item['price']
+				ret = True
+				successful_matched = True
+		if not successful_matched:
+			if taobao_desc not in describe_type:
+				continue
+			fout.write('"'+str(cid)+'",' # cid
+			+'"'+taobao_trans_time+'",' # time
+			+'"'+str(item['price'])+'",' # money
+			+'"'+' '+'",' # status
+			+'"'+' '+'",' # ali_desc
+			+'"'+item['describe']+'",' # tb_desc
+			+'"'+item['type']+'",' # type
+			+'"'+str(item['quantity'])+'",' #quantity
+			+'"'+item['province']+'",' # tb_province
+			+'"'+item['city']+'",' # tb_city
+			+'"'+str(taobao_count)+'",' # alipay_count
+			+'"'+str(0)+'",' # tb_count
+			+'"'+huabeiAvailable+'",' #huabei_available
+			+'"'+taobaoLevel+'",' # taobaoleve;
+			+'"'+huabeiedu+'",' # huabeiedu
+			+'"'+describe_type[taobao_desc]+'"' #category
+			+'\n')
+
+	return ret
+
+
+for cnt,line in enumerate(open(input_filename)):
+	data = json.loads(line)
+	analysis(data['taobao_orders'], data['alipay_bills'], data['cid'], data['taobaoLevel'], data['huabeiedu'], data['huabeiAvailable'])
+	print cnt
+
+fout.close()
+flog.close()
